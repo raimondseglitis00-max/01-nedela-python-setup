e90960d8012befc31c9741823012ab6823ac48a1
Raimonds Eglitis <raimondseglitis00@gmail.com>
2026-03-01 17:58:11 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+print('Hello from VS Code')
+print('This is a Python file')
+print('You can run this file in the terminal or use the Run button in VS Code')
+
+
+print("Sviestmaizes pagatavošana")
+print("Grauzdē maizi...")
+sviests = input("Vai uzsmērēt sviestu? (jā/nē): ")
+if sviests.lower() == "jā":
+    print("Sviests uzsmērēts!")
+desa = input("Vai pievienot desu? (jā/nē): ")
+if desa.lower() == "jā":
+    print("Desa pievienota!")
+siers = input("Vai pievienot sieru? (jā/nē): ")
+if siers.lower() == "jā":
+    print("Siers pievienots!")
+print("Sviestmaize gatava! Baudiet!")
